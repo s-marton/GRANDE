bab760f24f83335aba5fc8571f11e79c80132884
Sascha Marton <33120125+s-marton@users.noreply.github.com>
2023-09-27 15:38:13 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 read -n1 -p "Do you want to install project specific packages? [y,n]" doit 
 case $doit in  
-    y|Y) cat ./relevant_dependencies.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 python -m pip install ;;
+    y|Y)
+    cat ./relevant_dependencies.txt | sed -e '/^\s*#.*$/d' -e '/^\s*$/d' | xargs -n 1 python -m pip install 
+    conda install -y graphviz
+    ;;
     n|N) echo '' ; echo Nothing to install. Done. ;; 
     *) echo '' ; echo Wrong input. Please try again. ;; 
 esac
