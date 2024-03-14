5771d3c538578d0a67927fa2f779fb0259ab3b9e
Chowkah <sascha.marton@gmail.com>
2024-03-14 12:31:14 +0100

    10x speedup for training, restructuring, model saving

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="GRANDE",
-    version="0.1.2",
+    license="MIT",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
-        'focal_loss>=0.0.7',
-        'category_encoders>=2.6.1',
+        'category_encoders==2.6.1',
         'pandas>=2.0.3',
-        'tensorflow_addons>=0.21.0',
         'tensorflow==2.13',
-        'numpy>=1.23.2',
+        'numpy>=1.24.3',
         'scikit_learn>=1.3.0',
+        'focal_loss==0.0.7',
     ],
     author="Sascha Marton",
     author_email="sascha.marton@gmail.com",
