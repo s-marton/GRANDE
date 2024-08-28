684c9b8e4dd87353eb3160bcd577da8d66d47671
Sascha Marton <sascha.marton@gmail.com>
2024-08-28 09:41:23 +0200

    Bugfix (regression + LOO)
    Use normalized target for LOO as high target values can screw up the model training

--- GRANDE/GRANDE.py
+++ GRANDE/GRANDE.py
@@ -193,7 +193,10 @@ class GRANDE(tf.keras.Model):
         X_val = self.encoder_ordinal.transform(X_val)     
         
         self.encoder_loo = ce.LeaveOneOutEncoder(cols=high_cardinality_indices)
-        self.encoder_loo.fit(X_train, y_train)
+        if self.objective == 'regression':
+            self.encoder_loo.fit(X_train, (y_train-self.mean)/self.std)
+        else:
+            self.encoder_loo.fit(X_train, y_train)                                 
         X_train = self.encoder_loo.transform(X_train)
         X_val = self.encoder_loo.transform(X_val)
         
